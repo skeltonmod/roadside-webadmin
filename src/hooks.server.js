c1e0ace1dd9859806f0a7f24d8ce087a3d119e45
skeltonmod <spacecat34@gmail.com>
2023-12-24 17:42:48 +0800

    FIX Cors

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,24 @@
-import { sequence } from "@sveltejs/kit/hooks"
-import { auth } from "./utils/lucia"
+import { sequence } from '@sveltejs/kit/hooks';
+import { auth } from './utils/lucia';
 export const handle = async ({ event, resolve }) => {
-	// we can pass `event` because we used the SvelteKit middleware
 	event.locals.auth = auth.handleRequest(event);
-	return await resolve(event);
+
+	if (event.url.pathname.startsWith('/api')) {
+		// Required for CORS to work
+		if (event.request.method === 'OPTIONS') {
+			return new Response(null, {
+				headers: {
+					'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*'
+				}
+			});
+		}
+	}
+
+	const response = await resolve(event);
+	if (event.url.pathname.startsWith('/api')) {
+		response.headers.append('Access-Control-Allow-Origin', `*`);
+	}
+	return response;
 };
--- src/routes/api/auth/check-auth/+server.js
+++ src/routes/api/auth/check-auth/+server.js
@@ -2,8 +2,7 @@ import { auth } from '../../../../utils/lucia';
 import { fail, json } from '@sveltejs/kit';
 /** @type {import('./$types').RequestHandler} */
 export async function GET({request, cookies}) {
-    const authRequest = auth.handleRequest({request: request, cookies});
+    const authRequest = auth.handleRequest({request, cookies});
     const session = await authRequest.validateBearerToken();
-
     return json(session)
 }
