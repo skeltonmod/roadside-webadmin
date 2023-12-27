c15bd32bb67d3c610924a52fea9ee8fdf85d1dd1
skeltonmod <spacecat34@gmail.com>
2023-12-27 11:24:29 +0800

    Append image on request

--- src/routes/api/car/[id]/+server.js
+++ src/routes/api/car/[id]/+server.js
@@ -1,16 +1,17 @@
 import { json, error } from "@sveltejs/kit";
 import { auth } from "../../../../utils/lucia";
 import prisma from "../../../../utils/client";
+import supabase from "../../../../utils/supabase";
 
-export async function GET({request, cookies}){
+export async function GET({request, cookies, params}){
     const authRequest = auth.handleRequest({request, cookies});
     const session = await authRequest.validateBearerToken();
 
     if(!session){
         throw error(401, 'Forbidden');
     }
-    const id = request.params.id;
-
+    const id = params.id;
+    let image = null;
     const car = await prisma.cars.findUnique({
         where: {
             id,
@@ -18,5 +19,10 @@ export async function GET({request, cookies}){
         }
     });
 
-    return json(car);
+    if(car?.image){
+        const {data} = await supabase.storage.from('roadside-images').createSignedUrl(car.image, 360);
+        image = data?.signedUrl;
+    }
+
+    return json({...car, image});
 }
