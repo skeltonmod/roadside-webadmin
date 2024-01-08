c4553129ce338d24552943f3a34a7a11274378c4
skeltonmod <spacecat34@gmail.com>
2024-01-08 14:49:32 +0800

    Fetch History API

---
+++ src/routes/api/request/history/+server.js
@@ -0,0 +1,40 @@
+import { json, error } from '@sveltejs/kit';
+import { auth } from '../../../../utils/lucia.js';
+import prisma from '../../../../utils/client.js';
+
+export async function GET({request, cookies}){
+    const authRequest = auth.handleRequest({ request, cookies });
+	const session = await authRequest.validateBearerToken();
+
+    if (!session) {
+        throw error(401, 'Forbidden');
+    }
+
+    const details = await prisma.userDetail.findUnique({
+		where: {
+			user_id: session.user.userId
+		}
+	});
+
+    const data = await prisma.request.findMany({
+		where: {
+			mechanic_id: details?.role == 'mechanic' ? session.user.userId : undefined,
+			user_id: details?.role == 'owner' ? session.user.userId : undefined,
+		},
+		include: {
+			car: true,
+			user: {
+				include: {
+					details: true
+				}
+			},
+			mechanic: {
+				include: {
+					details: true
+				}
+			}
+		}
+	});
+
+    return json(data);
+}
