4980d67e7bee28983c2bbd7863192a01c4704a3f
skeltonmod <spacecat34@gmail.com>
2024-01-20 18:45:13 +0800

    fucking shit fixes

--- src/routes/api/car/+server.js
+++ src/routes/api/car/+server.js
@@ -1,20 +1,27 @@
-import { json, error } from "@sveltejs/kit";
-import { auth } from "../../../utils/lucia.js";
-import prisma from "../../../utils/client.js";
+import { json, error } from '@sveltejs/kit';
+import { auth } from '../../../utils/lucia.js';
+import prisma from '../../../utils/client.js';
+import supabase from '../../../utils/supabase.js'
 
-export async function GET({request, cookies}){
-    const authRequest = auth.handleRequest({request, cookies});
-    const session = await authRequest.validateBearerToken();
+export async function GET({ request, cookies }) {
+	const authRequest = auth.handleRequest({ request, cookies });
+	const session = await authRequest.validateBearerToken();
+	const response = [];
+	if (!session) {
+		throw error(401, 'Forbidden');
+	}
 
-    if(!session){
-        throw error(401, 'Forbidden');
-    }
+	const cars = await prisma.cars.findMany({
+		where: {
+			user_id: session.user.userId
+		}
+	});
 
-    const cars = await prisma.cars.findMany({
-        where: {
-            user_id: session.user.userId
-        }
-    });
+	for (let i = 0; i < cars.length; ++i) {
+        console.log(cars[i].image);
+		const { data } = supabase.storage.from('roadside-images').getPublicUrl(cars[i].image);
+		cars[i].image = data.publicUrl;
+	}
 
-    return json(cars)
+	return json(cars);
 }
