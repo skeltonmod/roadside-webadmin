f3707e1c1669c559520fef1e450cec08b690bc9c
skeltonmod <spacecat34@gmail.com>
2024-01-22 20:22:56 +0800

    pussyhole

--- src/routes/api/users/+server.js
+++ src/routes/api/users/+server.js
@@ -1,6 +1,7 @@
 import { json, error } from "@sveltejs/kit";
 import { auth } from "../../../utils/lucia";
 import prisma from "../../../utils/client";
+import supabase from "../../../utils/supabase.js";
 
 export async function GET({request, cookies}){
     const authRequest = auth.handleRequest({ request, cookies });
@@ -21,6 +22,22 @@ export async function GET({request, cookies}){
             }
         }
     });
+
+    for(let i = 0; i < users.length; ++i){
+        const user = users[i];
+        if(user.details.details.id_image){
+            // console.log(user.details.details.id_image)
+            const { data } = supabase.storage.from('roadside-images').getPublicUrl(user.details.details.id_image.path);
+            user.details.details.id_image = data.publicUrl
+        }
+        if(user.details.details.permit_image){
+            const { data } = supabase.storage.from('roadside-images').getPublicUrl(user.details.details.permit_image.path);
+            user.details.details.permit_image = data.publicUrl
+        }
+        console.log(user.details);
+    }
+
+    
 
     return json(users);
 }
