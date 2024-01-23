32fd0fb86afed95dd4d5e90351aaaa3b72eb9c70
skeltonmod <spacecat34@gmail.com>
2024-01-23 12:14:23 +0800

    WOWOWOW

--- prisma/migrations/20240119094557_add_on_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `email_verification_token` DROP FOREIGN KEY `email_verification_token_user_id_fkey`;
-
--- AddForeignKey
-ALTER TABLE `email_verification_token` ADD CONSTRAINT `email_verification_token_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
