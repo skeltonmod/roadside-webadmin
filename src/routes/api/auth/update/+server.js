637811ca75bfe31dea131623bec024e6ed8f0339
Free Wifi <gil.pacturan@fligno.com>
2024-01-23 13:41:25 +0800

    MModified update

--- src/routes/api/auth/update/+server.js
+++ src/routes/api/auth/update/+server.js
@@ -2,21 +2,30 @@ import prisma from "../../../../utils/client";
 import { auth } from "../../../../utils/lucia";
 import { error, json } from "@sveltejs/kit";
 
-export async function POST({request, cookies}){
-    const {details} = await request.json();
-    const authRequest = auth.handleRequest({request, cookies});
+export async function POST({ request, cookies }) {
+    const { details } = await request.json();
+    const authRequest = auth.handleRequest({ request, cookies });
     const session = await authRequest.validateBearerToken();
 
-    if(!session){
+    if (!session) {
         throw error(401, 'Forbidden');
     }
 
-    const user = await prisma.userDetail.update({
+    const updated = await prisma.userDetail.update({
         where: {
             user_id: session.user.userId
         },
         data: {
             details
+        }
+    });
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: session.user.userId
+        },
+        include: {
+            details: true
         }
     });
 
