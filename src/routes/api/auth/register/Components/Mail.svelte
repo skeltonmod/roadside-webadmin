de2ef1a1d4d214cfb0bdc63bcdcdffe1842c18cd
novidash <abgaoe@gmail.com>
2024-01-30 23:01:22 +0800

    Update Mail.svelte

--- src/routes/api/auth/register/Components/Mail.svelte
+++ src/routes/api/auth/register/Components/Mail.svelte
@@ -7,4 +7,4 @@
 
 <h4>Welome to Roadside Assistance {name}</h4>
 
-<a href={`http://localhost:5173/api/verify/${token}`}>Click here to verify your account</a>
+<a href={`https://roadside-webadmin.vercel.app/api/verify/${token}`}>Click here to verify your account</a>
