b5bd6995b71a8b6cde8bff686eb4152320cb588b
skeltonmod <spacecat34@gmail.com>
2024-02-03 01:13:17 +0800

    Dogshit

--- src/routes/email-verification/+page.svelte
+++ src/routes/email-verification/+page.svelte
@@ -1,6 +1,28 @@
 <script>
-    
-</script>
-
-
-<h1>YOU ARE NOW VERIFIED, WOW!</h1>
+    // You can add script logic here if needed
+    import image from '../logo.webp'
+  </script>
+  
+  <style>
+    /* Center the content */
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh; /* Ensure full viewport height */
+    }
+  
+    /* Adjust image size */
+    img {
+      max-width: 50%; /* Set maximum width to the container width */
+      height: auto; /* Maintain aspect ratio */
+      margin-bottom: 10px;  
+    }
+  </style>
+  
+  <div>
+    <!-- Success logo -->
+    <img src={image} alt="Success Logo" />
+  
+  </div>
